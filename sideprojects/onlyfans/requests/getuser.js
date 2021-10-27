d9859d1d5a05ab71d2b55522b541503f28181e24
conto <constantijn@vanhartesveldt.nl>
2021-10-27 08:51:12 +0200

    added requirements

--- sideprojects/onlyfans/requests/getuser.js
+++ sideprojects/onlyfans/requests/getuser.js
@@ -46,27 +46,35 @@ var check = (hash) => {
   ).toString(16);
 };
 console.log(new Date().getTime())
-const config = {
+var config = {
   method: 'get',
-  url: 'https://onlyfans.com/api2/v2/posts?limit=10&offset=0&skip_users_dups=1&format=infinite',
+  url: 'https://onlyfans.com/api2/v2/users/15585607/posts?limit=10&order=publish_date_desc&skip_users=all&skip_users_dups=1&pinned=0&format=infinite',
   headers: { 
-    'app-token': '33d57ade8c02dbc5a333db99ff9ae26a', 
-    'Referer': 'https://onlyfans.com/', 
-    'sign': `1292:${bcTokenSha}:${check(bcTokenSha)}:61747246`, 
-    'x-bc': 'f5644d96593813ace2697be9b2cd92b5296d2571', 
+    'host': 'onlyfans.com', 
+    'connection': 'keep-alive', 
     'user-id': '83096664', 
-    'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:93.0) Gecko/20100101 Firefox/93.0', 
-    'auth_id': '83096664', 
-    'Accept': 'application/json', 
-    'Cookie': 'csrf=XXn6wp6I9545437a6f946edf9765bf41faf42d1b; fp=126d0e189b5b33e6d2433396b1ad5d35; ref_src=https%3A%2F%2Faccounts.google.com%2F; auth_id=83096664; st=730878b872d1f53f3ee30973e45aa0219456204ee3c2f82d0c733deead32d5b0; sess=kqgmv520i469hrmpkgn9eqimub',
-    'time': `${new Date().getTime()}`
+    'time': '1635192249954', 
+    'sec-ch-ua-mobile': '?0', 
+    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36', 
+    'app-token': '33d57ade8c02dbc5a333db99ff9ae26a', 
+    'accept': 'application/json, text/plain, */*', 
+    'x-bc': '2f593e32e6cc8aa1d10264400427d079d9bed082', 
+    'sec-ch-ua': '"Google Chrome";v="95", "Chromium";v="95", ";Not A Brand";v="99"', 
+    'sign': '1307:9e3182bd7485bcbd5528effa5c3390aacd8a842b:bc8:6176c656', 
+    'sec-ch-ua-platform': '"Linux"', 
+    'sec-fetch-site': 'same-origin', 
+    'sec-fetch-mode': 'cors', 
+    'sec-fetch-dest': 'empty', 
+    'referer': 'https://onlyfans.com/onlyfans', 
+    'accept-encoding': 'gzip, deflate, br', 
+    'accept-language': 'en-US,en;q=0.9', 
+    'cookie': 'csrf=9FXKQpku9fc7979f0a5e714fb7ef66fb2f825470; fp=e2241d978f3d41e27fcad987284946a1; auth_id=83096664; ref_src=https%3A%2F%2Fonlyfans.com%2F; sess=h83g0mp26oaj438j43leu412d5; st=4705a99a9c6b28914d6ab776ac468db072c4a0f2e19f6a33d42e455eac6281d1; sess=h83g0mp26oaj438j43leu412d5', 
   }
 };
 
 axios(config)
 .then(function (response) {
-  console.log(response);
-  console.log(response.body);
+  console.log(JSON.stringify(response.data));
 })
 .catch(function (error) {
   console.log(error);
