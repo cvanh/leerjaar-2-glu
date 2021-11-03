3d3cddf66428af47c672b469fe86c5bcd067e1f2
constantijn van hartesveldt <constantijn@vanhartesveldt.nl>
2021-11-03 11:15:01 +0100

    working

--- periode5/database/2b/server.js
+++ periode5/database/2b/server.js
@@ -28,52 +28,6 @@ conn.connect(function (err) {
   console.log("Connected to the MySQL server.");
 });
 
-// app.get("/read/:id",jsonParser, async (req, res) => {
-//   await authcheck(req,res,()=>{
-//     conn.query(
-//       "SELECT * FROM `product` WHERE id = " + parseInt(req.params.id),
-//       (error, results, fields) => {
-//         console.log(results);
-//         if (results.length === 0) { // result is a array and if it array is empty it is length is 0
-//           // nothing found sending http code 204 for no content
-//           res.status(g).send(results);
-//         } else {
-//           // found something
-//           res.status(200).send(results);
-//         }
-//       }
-//     );
-//   })
-// });
-
-// /**
-//  * @description add a product into the database
-//  */
-// app.post("/add", jsonParser, async (req, res) => {
-//   console.log(req.body);
-//   conn.query(`INSERT INTO product (category_id, naam, beschrijving, prijs, toegevoegd_op, gewijzigd_op) VALUES ( '${req.body.category_id}', '${req.body.naam}', '${req.body.beschrijving}', '${req.body.prijs}', '${req.body.toegevoegd_op}', '${req.body.gewijzigd_op}')`);
-//   res.status(200).send();
-// });
-
-// /**
-//  * @description updates a field based on: id, row and the new value
-//  * @ussage in the post request body send: {"id": TheID, "row":Row, "value": Value }
-//  */
-// app.post("/update", jsonParser, async (req, res) => {
-//   console.log(req.body);
-//   conn.query(
-//     `UPDATE product SET ${req.body.row} = '${req.body.value}' WHERE id = ${req.body.id}; `
-//   );
-//   res.status(200).send();
-// });
-
-// app.get("/delete/:table/:id", jsonParser, async (req, res) => {
-//   console.log(
-//     `deleting row with id: ${req.params.id} from the table: ${req.params.table}`
-//   );
-//   conn.query(
-//     `DELETE FROM ${req.params.table} WHERE id = ${req.params.id}`);
-// });
 app.use("/auth",require("./router/auth.js"))
 app.use("/api2/product",require("./router/api.js"))
 
