c92868763abc520672007a3dbd11e8ae11cac14e
conto <constantijn@vanhartesveldt.nl>
2021-11-04 10:48:32 +0100

    working

--- periode5/php/eind/curse.php
+++ periode5/php/eind/curse.php
@@ -1,8 +1,10 @@
 <?php
+
 /**
  * class with all the functions for cursecheck
  */
-class Curse {
+class Curse
+{
     protected int $CurseBump;
     /**
      * @function _construct
@@ -12,33 +14,59 @@ class Curse {
         $Sentence,
         $CursewordsList,
         $CurseBump
-        ){
-        $this->CurseCheck($Sentence,$CursewordsList,$CurseBump);
+    ) {
+        $this->CurseCheck($Sentence, $CursewordsList, $CurseBump);
     }
     /**
      * @param 
      */
-    private function CurseCheck($Sentence,$CursewordsList,$CurseBump) {
+    private function CurseCheck($Sentence, $CursewordsList, $CurseBump)
+    {
         $Words = $this->SplitSentence($Sentence);
-        $good = "bleep";
-        echo str_ireplace($CursewordsList,$good,$Sentence);
+        $good = "BleeP";
+
+        // due to limitations of php we need to check the value less compact
+        switch ($CurseBump) {
+            case 1:
+                echo "a";
+                // 1 = Alle mogelijk scheldwoorden filteren,  
+                $t = $this->FilterSentence($Sentence);
+                echo str_ireplace($CursewordsList, $good, $t);
+                break;
+            case 2:
+                echo "b";
+                //     2 = Alleen goed geschreven scheldwoorden 
+                echo str_replace($CursewordsList, $good, $Sentence);
+
+                break;
+
+            case 3:
+                echo $Sentence;
+                //     3 = Niet filteren 
+
+
+                break;
+            default:
+                echo "no valid cursechecklevel found please provide one in the url";
+        }
+        
     }
 
-    // 3
-    private function SplitSentence($Sentence){
-        $t = $this->FilterSentence($Sentence);
-        $Words = explode(" ", $t);
+    private function SplitSentence($Sentence)
+    {
+        $Words = explode(" ", $Sentence);
         // var_dump($Words);
         return $Words;
     }
 
-    private function FilterSentence($Sentence){
-        return strtolower($Sentence);        
+    private function FilterSentence($Sentence)
+    {
+        return strtolower($Sentence);
     }
 }
 
 $Sentence = "de KaNker poes springt uit de kut boom met zijn tering pootjes";
-$CursewordsList = ["kanker", "kaas","kut"];
-$CurseBump = 70;
+$CursewordsList = ["kanker", "kaas", "kut", "tering"];
+$CurseBump = $_GET["cursecheckvalue"];
 
-$lihfds = new Curse($Sentence,$CursewordsList,$CurseBump);
+$lihfds = new Curse($Sentence, $CursewordsList, $CurseBump);
--- periode5/php/eind/readme.md
+++ periode5/php/eind/readme.md
@@ -1,1 +1,74 @@
 dick in toaster
+
+### assignment 
+
+
+Voor deze opdracht ga je een API maken waar je zinnen heen stuurt. Deze controleert op scheldwoorden en filtert deze uit de tekst.  
+
+De zinnen worden gefilterd op scheldwoorden. De scheldwoorden worden dan verwijderd uit de tekst en vervangen voor een alternatief. Dit alternatief kan een developer zelf opgeven. 
+ 
+Eisen voor het product: 
+
+ 
+
+Scheldwoord eisen database 
+
+    id 
+    Woord 
+    Goedgekeurd 
+    Gradatie scheldwoord 
+
+ 
+
+CRUD systeem voor woorden-beheer 
+
+ 
+
+API moet configuratie hebben: 
+
+    Woord vervangen door (zelf waarde invullen) 
+    Gradatie van scheldwoorden  
+    1 = Alle mogelijk scheldwoorden filteren,  
+    2 = Alleen goed geschreven scheldwoorden 
+    3 = Niet filteren 
+
+ 
+Werkwijze 
+
+Stap 1: 
+
+Maak een class schema. 
+
+Dit class schema laten goedkeuren door docent. 
+
+Stap 2: 
+
+Bouwen applicatie. 
+
+Stap 3: 
+
+Elkaar beoordelen met punten schema. Peer reviews. 
+Beoordeling 
+
+Overzicht van punten
+ 
+
+Velden in het blauw zijn extra  
+
+Basis vlakken = 34 punten 
+
+EXTRA vlakken = 16 punten 
+
+Totaal 50 punten 
+
+ 
+
+Je hebt voor de opdracht 34 punten nodig voor een 8. 
+
+Formule: 
+
+Cijfer = ( (Aantal punten) / 34) * 7 + 1 
+
+ 
+
+Mensen die boven een 10 halen, krijgen een verrassing 
