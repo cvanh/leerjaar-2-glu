e84e58f076dcb5bf1c966ca2240091d0d2d4fd34
conto <constantijn@vanhartesveldt.nl>
2021-11-26 00:02:15 +0100

    worki

--- sideprojects/rust/foo/target/rls/debug/deps/foo-eb7f769bb129d9db.d
+++
@@ -1,5 +0,0 @@
-c:\Users\cvanh\Desktop\leerjaar-2-glu\sideprojects\rust\foo\target\rls\debug\deps\foo-eb7f769bb129d9db.rmeta: src\main.rs
-
-c:\Users\cvanh\Desktop\leerjaar-2-glu\sideprojects\rust\foo\target\rls\debug\deps\foo-eb7f769bb129d9db.d: src\main.rs
-
-src\main.rs:
