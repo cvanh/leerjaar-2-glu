b99efee74ea1c56be9f83fc82f098cc1597cd214
constantijn van hartesveldt <constantijn@vanhartesveldt.nl>
2021-12-15 17:40:19 +0100

    work

--- periode6/portofolio/nextjs-blog/components/Home.js
+++ periode6/portofolio/nextjs-blog/components/Home.js
@@ -1,4 +1,7 @@
+import React from 'react'
+
 function Home(){
+    alert(1)
     return(
         <h1>kaas</h1>
     )
--- periode6/portofolio/nextjs-blog/pages/index.js
+++ periode6/portofolio/nextjs-blog/pages/index.js
@@ -1,39 +1,38 @@
-<<<<<<< HEAD
-// function Page({ stars }) {
-//   return <div>Next stars: {stars}</div>
-// }
-
-// Page.getInitialProps = async (ctx) => {
-//   const res = await fetch('https://api.github.com/repos/vercel/next.js')
-//   const json = await res.json()
-//   return { stars: json.stargazers_count }
-// }
-
-import { useRouter } from "next/router";
+import { ThemeProvider, Preflight } from "@xstyled/styled-components";
+import { Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link,
+  Redirect,
+} from "react-router-dom";
 import Home from "../components/Home";
 import Test from "../components/Test";
-=======
-import React from 'react'
-function Page({ stars }) {
-  return <div>
-    <div>Next stars: {stars}</div>
-    <a href="/admin/createblog">kaas</a>
-  </div>
-}
->>>>>>> 29e50486d9e1fd000d20f886652d90a348e7e7be
+import theme from "../utils/theme";
 
-function Page({ uri, page, error, cookies, preview  }) {
-  const getPageBasedOnTemplate = (template) => {
-  switch (page) {
-    case "test":
-      return <Test  />;
-    case "home":
-      return <Home />;
-    default:
-      return <Home />;
-  }
-  }
-  return getPageBasedOnTemplate(page?.pageTemplate);
+function Page({Component}) {
+  // if (typeof window === 'undefined') {
+  //   return null
+  // }
+
+  return (
+    <ThemeProvider theme={theme}>
+
+      <Router>
+          <Switch>
+
+        <Route path="/" component={Home}/>
+      
+        <Route path="/home" component={Home} />
+      
+        <Route path="/test" component={Test} />
+        
+        </Switch>
+      </Router>
+
+    </ThemeProvider>
+  );
 }
 
 export default Page;
